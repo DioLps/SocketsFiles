edb8120eab5f04bbf9bec3a803dc5313c910f4f1
diolps <diolopesrln@gmail.com>
2018-03-03 09:56:46 -0300

    first commit1

---
+++ src/sockets/MainCliente.java
@@ -0,0 +1,21 @@
+package sockets;
+
+/**
+ *
+ * @author diolps
+ */
+public class MainCliente {
+
+    public static void main(String[] args) {
+        
+        //Declaração de escopo
+                
+        //Etapa pedido de conexão - Utilizar porta 9876 e ip da máquina onde o servidor está rodando.
+        
+        //Etapa de comunicacao - Onde será enviada a mensagem ao servidor.
+        
+        //Encerramento da conexão
+        
+    }
+
+}
---
+++ src/sockets/MainServidor.java
@@ -0,0 +1,22 @@
+package sockets;
+
+/**
+ *
+ * @author diolps
+ */
+public class MainServidor {
+
+    public static void main(String[] args) {
+
+        //Declaração de escopo
+
+        //Criação do socket Servidor e bind
+
+        //Espera pelo pedido do cliente: accept        
+
+        //Etapa de comunicação
+        
+        //Encerramento da conexão
+        
+    }
+}
